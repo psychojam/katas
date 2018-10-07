75255ca211b7821442e24f9beb97970fa6d8894b
psychojam <djamel.bouchoucha.auditeur@lecnam.net>
2018-10-07 15:11:08 +0200

    fix test

--- CSharp/CSharp_Katas/Step1_Algo.Tests/Math.cs
+++ CSharp/CSharp_Katas/Step1_Algo.Tests/Math.cs
@@ -9,14 +9,14 @@ namespace Step1_Algo.Tests
         [TestMethod]
         public void Number_6_is_not_odd_number()
         {
-            var isLeap = Step1_Algo.Leap.IsLeapYear(6);
+            var isLeap = Step1_Algo.Math.IsOdd(6);
             Assert.IsFalse(isLeap);
         }
 
         [TestMethod]
         public void Number_5_is_odd_number()
         {
-            var isLeap = Step1_Algo.Leap.IsLeapYear(5);
+            var isLeap = Step1_Algo.Math.IsOdd(5);
             Assert.IsTrue(isLeap);
         }
 

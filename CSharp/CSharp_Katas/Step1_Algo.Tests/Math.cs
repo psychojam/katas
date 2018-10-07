c915c6b4e319b5e3b9b56ff2c223c4f4165d023d
psychojam <djamel.bouchoucha.auditeur@lecnam.net>
2018-10-07 18:41:53 +0200

    fix variables name

--- CSharp/CSharp_Katas/Step1_Algo.Tests/Math.cs
+++ CSharp/CSharp_Katas/Step1_Algo.Tests/Math.cs
@@ -5,19 +5,18 @@ namespace Step1_Algo.Tests
     [TestClass]
     public class Math
     {
-        // Odd
         [TestMethod]
         public void Number_6_is_not_odd_number()
         {
-            var isLeap = Step1_Algo.Math.IsOdd(6);
-            Assert.IsFalse(isLeap);
+            var isOdd = Step1_Algo.Math.IsOdd(6);
+            Assert.IsFalse(isOdd);
         }
 
         [TestMethod]
         public void Number_5_is_odd_number()
         {
-            var isLeap = Step1_Algo.Math.IsOdd(5);
-            Assert.IsTrue(isLeap);
+            var isOdd = Step1_Algo.Math.IsOdd(5);
+            Assert.IsTrue(isOdd);
         }
 
     }
